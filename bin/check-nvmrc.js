0369c31e23770ba045bfd4b4079d1e7738663a5b
AJ <andrew.hosgood@landregistry.gov.uk>
2022-07-19 14:37:08 +0100

    Prettier

--- bin/check-nvmrc.js
+++ bin/check-nvmrc.js
@@ -1,41 +1,55 @@
 #! /usr/bin/env node
 
-'use strict'
+"use strict";
 
-var fs = require('fs')
-var path = require('path')
+var fs = require("fs");
+var path = require("path");
 
-fs.readFile(path.join(__dirname, '../.nvmrc'), 'utf8', function (error, data) {
-  if (error) throw error
-  var expectedVersion = data.trim()
-  var currentVersion = process.version.replace('v', '')
+fs.readFile(path.join(__dirname, "../.nvmrc"), "utf8", function (error, data) {
+  if (error) throw error;
+  var expectedVersion = data.trim();
+  var currentVersion = process.version.replace("v", "");
 
-  var versionMatchesExactly = expectedVersion === currentVersion
-  var versionMatchesMajor = expectedVersion.split('.')[0] === currentVersion.split('.')[0]
+  var versionMatchesExactly = expectedVersion === currentVersion;
+  var versionMatchesMajor =
+    expectedVersion.split(".")[0] === currentVersion.split(".")[0];
 
   if (versionMatchesExactly) {
-    process.exit()
+    process.exit();
   }
 
-  var nvmInstallText = 'To do this you can install nvm (https://github.com/creationix/nvm) then run `nvm install`.'
+  var nvmInstallText =
+    "To do this you can install nvm (https://github.com/creationix/nvm) then run `nvm install`.";
 
   if (versionMatchesMajor) {
-    console.log('' +
-      'Warning: You are using Node.js version ' + currentVersion + ' which we do not use. ' +
-      '\n\n' +
-      'You may encounter issues, consider installing Node.js version ' + expectedVersion + '.' +
-      '\n\n' +
-      nvmInstallText +
-    '')
-    process.exit()
+    console.log(
+      "" +
+        "Warning: You are using Node.js version " +
+        currentVersion +
+        " which we do not use. " +
+        "\n\n" +
+        "You may encounter issues, consider installing Node.js version " +
+        expectedVersion +
+        "." +
+        "\n\n" +
+        nvmInstallText +
+        ""
+    );
+    process.exit();
   }
 
-  console.log('' +
-    'You are using Node.js version ' + currentVersion + ' which we do not support. ' +
-    '\n\n' +
-    'Please install Node.js version ' + expectedVersion + ' and try again.' +
-    '\n\n' +
-    nvmInstallText +
-  '')
-  process.exit(1) // exit with a failure mode
-})
+  console.log(
+    "" +
+      "You are using Node.js version " +
+      currentVersion +
+      " which we do not support. " +
+      "\n\n" +
+      "Please install Node.js version " +
+      expectedVersion +
+      " and try again." +
+      "\n\n" +
+      nvmInstallText +
+      ""
+  );
+  process.exit(1); // exit with a failure mode
+});
