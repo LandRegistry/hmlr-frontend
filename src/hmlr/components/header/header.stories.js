dd6c9752016742fdf37a4f9770e020c541b17d59
AJ <andrew.hosgood@landregistry.gov.uk>
2022-07-21 16:53:05 +0100

    Remove custom button, use GDS header

--- src/hmlr/components/header/header.stories.js
+++ src/hmlr/components/header/header.stories.js
@@ -3,14 +3,53 @@ import "../../all.scss";
 import "./_header.scss";
 
 export default {
-  title: "Experimental/Header",
+  title: "Components/Header",
   argTypes: {
+    homepageUrl: { control: "text" },
+    assetsPath: { control: "text" },
+    productName: { control: "text" },
     serviceName: { control: "text" },
+    serviceUrl: { control: "text" },
+    navigation: { control: "object" },
+    navigationClasses: { control: "text" },
+    navigationLabel: { control: "text" },
+    menuButtonLabel: { control: "text" },
+    containerClasses: { control: "text" },
+    classes: { control: "text" },
+    attributes: { control: "text" },
   },
 };
 
-const Template = ({ serviceName }) => {
-  return Header({ params: { serviceName } });
+const Template = ({
+  homepageUrl,
+  assetsPath,
+  productName,
+  serviceName,
+  serviceUrl,
+  navigation,
+  navigationClasses,
+  navigationLabel,
+  menuButtonLabel,
+  containerClasses,
+  classes,
+  attributes,
+}) => {
+  return Header({
+    params: {
+      homepageUrl,
+      assetsPath,
+      productName,
+      serviceName,
+      serviceUrl,
+      navigation,
+      navigationClasses,
+      navigationLabel,
+      menuButtonLabel,
+      containerClasses,
+      classes,
+      attributes,
+    },
+  });
 };
 
 export const Standard = Template.bind({});
