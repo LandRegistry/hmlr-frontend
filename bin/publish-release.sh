158950089290e7085bd750a0726c08289109b23e
AJ <andrew.hosgood@landregistry.gov.uk>
2022-08-31 20:03:19 +0100

    Tidy publish script

--- bin/publish-release.sh
+++ bin/publish-release.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 set -e
 
+if [ $(git diff --quiet HEAD) ]; then
+    echo " "
+    echo "Uncommited changes detected - commit all changes before publishing"
+fi
+
 source ./bin/build-release.sh
 source ./bin/generate-npm-tag.sh
 
@@ -20,13 +25,11 @@ source ./bin/generate-npm-tag.sh
 echo " "
 echo "🟢  Starting a release..."
 echo "This will:"
-# echo "- check that you're logged in to npm as the correct user"
 echo "- publish the package to npm if it has not been published already"
 echo "- tag the package on npm with '$NPM_TAG'"
 echo "- check that there is not already a Github tag published"
 echo "- create a new v$CURRENT_VERSION tag"
 echo "- push the tag to remote origin"
-# echo "- create a zip file of the 'dist/' directory locally"
 echo " "
 
 read -r -p "Do you want to continue? [y/N] " continue_prompt
@@ -36,28 +39,15 @@ if [[ $continue_prompt != 'y' ]]; then
     exit 0
 fi
 
-# echo  "Checking that you can publish to npm..."
-
-# at some point we should create a team and check if user exists in a team
-# ! npm team ls developers | grep -q $NPM_USER
-
-# NPM_USER=$(npm whoami)
-# if ! [ "govuk-patterns-and-tools" == "$NPM_USER" ]; then
-#     echo "⚠️ FAILURE: You are not logged in with the correct user."
-#     exit 1
-# fi
-
 echo " "
 echo "📦  Publishing package..."
 
-# Try publishing
 cd package
 [ $NPM_TAG = "latest" ] && npm publish || npm publish --tag $NPM_TAG
 echo " "
 echo "📫  Package published!"
 cd ..
 
-# Extract tag version from ./package/package.json
 ALL_PACKAGE_VERSION=$(node -p "require('./package/package.json').version")
 TAG="v$ALL_PACKAGE_VERSION"
 
@@ -71,9 +61,4 @@ else
     git tag $TAG -m "HMLR Frontend release $TAG"
     git push --tags
     echo "Tag $TAG created and pushed to remote."
-
-    # echo "🗂 Creating a release artifact..."
-    # git archive -o ./release-$TAG.zip HEAD:package
-    # # git archive --format=zip HEAD:package > ./release-$TAG.zip
-    # echo "Artifact created. Now create a release on GitHub (https://github.com/LandRegistry/hmlr-frontend/releases/new?tag=$TAG) and attach this."
 fi
