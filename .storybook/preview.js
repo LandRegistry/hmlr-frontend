e2ac851f5eb895c1a6b3355378a3b6c839fdfed6
AJ <mail@andrewhosgood.com>
2023-03-30 13:52:18 +0100

    Change preview window padding and background to match GDS

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,5 @@
 document.documentElement.classList.add("govuk-template");
+document.documentElement.classList.add("app-example-page__wrapper");
 document.body.classList.add("govuk-template__body");
 document.body.classList.add("js-enabled");
 
